66abda4bb324790e412d53a9074f3af70fa39f66
Luis Henrique dos Santos Abrantes <luis.hsa@gmail.com>
2025-12-10 03:16:00 -0300

    Improve code formatting and add comments for clarity in fetch JSON example - type anotations. Finishing section 1

--- section01/fetchjson/index.ts
+++ section01/fetchjson/index.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+// type anotation to don't get messed up with the syntax calling the objects (1)
 interface Todo {
   id: number;
   title: string;
@@ -9,15 +10,20 @@ interface Todo {
 }
 
 axios.get(url).then(response => {
-  const todo = response.data as Todo;
+    const todo = response.data as Todo;
 
-  const id = todo.id;
-  const title = todo.title;
-  const finished = todo.completed;
+    const id = todo.id; // (1)
+    const title = todo.title; // (1)
+    const finished = todo.completed; // (1)
 
+    logTodo(id, title, finished); // (2)
+});
+
+// type anotation to don't get messed up with the order on logTodo call (2)
+const logTodo = (id: number, title: string, finished: boolean) => {
   console.log(`  
-    The Todo with ID: ${id}
+    The Todo with ID: ${id} 
     Has the title: ${title}
     Is it finished? ${finished}
-    `)
-});
+    `);
+}
